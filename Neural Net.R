4f3f35063fee1b2c9866bf8aa5f14e7eea8e68ec
swapnavijay01 <56800081+swapnavijay01@users.noreply.github.com>
2019-10-21 14:32:00 -0700

    r codes

---
+++ Neural Net.R
@@ -0,0 +1,68 @@
+# Artificial Neural Network
+
+# Importing the dataset
+dataset = read.csv('Churn_Modelling.csv')
+head(dataset)
+dataset = dataset[4:14]
+
+# Encoding the categorical variables as factors
+dataset$Geography = as.numeric(factor(dataset$Geography,
+                                      levels = c('France', 'Spain', 'Germany'),
+                                      labels = c(1, 2, 3)))
+dataset$Gender = as.numeric(factor(dataset$Gender,
+                                   levels = c('Female', 'Male'),
+                                   labels = c(1, 2)))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Exited, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[-11] = scale(training_set[-11])
+test_set[-11] = scale(test_set[-11])
+
+# Fitting ANN to the Training set
+#install.packages('h2o')
+library(h2o)
+h2o.init(nthreads = -1) #optimizes the number of cords to use
+model1 = ?h2o.deeplearning(y = 'Exited',
+                         training_frame = as.h2o(training_set),
+                         activation = 'Rectifier',
+                         hidden = c(6,6),
+                         epochs = 100,
+                         train_samples_per_iteration = -2,
+                         variable_importances=T)
+
+
+# dl.VI =my.dl@model1$varimp
+# print(dl.VI)
+install.packages('neuralnet')
+require(neuralnet)
+names(training_set)
+net <-neuralnet(training_set$Exited~CreditScore+Geography+Gender+Age, 
+                training_set, hidden=1,
+                        rep=10, err.fct="ce", 
+                  linear.output=FALSE)
+
+print(net)
+plot(net, rep="best")
+
+# Predicting the Test set results
+y_pred = h2o.predict(model, newdata = as.h2o(test_set[-11]))
+y_pred = (y_pred > 0.5)
+y_pred = as.vector(y_pred)
+
+# Making the Confusion Matrix
+cm = table(test_set[, 11], y_pred)
+
+h20.varimp
+h20.varimp_plot
+
+ h2o.shutdown()
+
+
+ 
